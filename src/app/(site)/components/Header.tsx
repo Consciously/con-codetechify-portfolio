9bf88f15ce4a9cf723c2356b6ca7a73656d40169
Stefan Ihle <stefanihle@gmx.net>
2023-06-20 11:13:08 +0200

    add navigation

--- src/app/(site)/components/Header.tsx
+++ src/app/(site)/components/Header.tsx
@@ -1,27 +1,110 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const router = useRouter();
+
+	const handleNavigation = (route: string) => {
+		router.push(route);
+		setIsOpen(preIsOpenState => !preIsOpenState);
+	};
+
 	return (
-		<header className='py-4 bg-slate-950 text-white'>
-			<div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-				<Link href='/' className='text-lg font-bold text-white uppercase'>
-					codetechify.com
-				</Link>
-				<nav>
-					<ul className='flex flex-col md:flex-row  md:space-x-4'>
-						<li>
-							<Link href='/projects' className='uppercase font-semibold'>
+		<header className='relative bg-slate-950 text-white'>
+			<div className='container mx-auto px-4 sm:px-6 lg:px-8'>
+				<div className='flex items-center justify-between h-16'>
+					<div className='flex items-center'>
+						<div className='flex-shrink-0'>
+							<Link href='/' className='text-lg font-bold text-white uppercase'>
+								codetechify.com
+							</Link>
+						</div>
+					</div>
+					<div className='hidden md:block'>
+						<div className='ml-10 flex items-baseline space-x-4'>
+							<Link
+								href='/projects'
+								className='uppercase font-semibold hover:text-rose-500'
+							>
 								Projects
 							</Link>
-						</li>
-						<li>
-							<Link href='/posts' className='uppercase font-semibold'>
+							<Link
+								href='/posts'
+								className='uppercase font-semibold hover:text-rose-500'
+							>
 								Blog
 							</Link>
-						</li>
-					</ul>
-				</nav>
+						</div>
+					</div>
+					<div className='-mr-2 flex md:hidden'>
+						<button
+							onClick={() => setIsOpen(!isOpen)}
+							type='button'
+							className='bg-slate-700 inline-flex items-center justify-center p-2 rounded-md text-slate-500 hover:text-white hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-800 focus:ring-white'
+							aria-controls='mobile-menu'
+							aria-expanded='false'
+						>
+							<span className='sr-only'>Open main menu</span>
+							{!isOpen ? (
+								<svg
+									className='block h-6 w-6'
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									stroke='currentColor'
+									aria-hidden='true'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth={2}
+										d='M4 6h16M4 12h16M4 18h16'
+									/>
+								</svg>
+							) : (
+								<svg
+									className='block h-6 w-6'
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									stroke='currentColor'
+									aria-hidden='true'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth={2}
+										d='M6 18L18 6M6 6l12 12'
+									/>
+								</svg>
+							)}
+						</button>
+					</div>
+				</div>
+			</div>
+
+			<div
+				className={`${isOpen ? 'block' : 'hidden'} md:hidden`}
+				id='mobile-menu'
+			>
+				<div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
+					<a
+						onClick={() => handleNavigation('/projects')}
+						className='uppercase text-semibold hover:text-rose-500 block px-3 py-2 rounded-md text-base font-medium'
+					>
+						Projects
+					</a>
+					<a
+						onClick={() => handleNavigation('/posts')}
+						className='uppercase text-semibold hover:text-rose-500 block px-3 py-2 rounded-md text-base font-medium'
+					>
+						Blog
+					</a>
+				</div>
 			</div>
 		</header>
 	);
