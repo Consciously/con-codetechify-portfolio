b534f0923a67bdb4c93e607c69c82aada6d70992
Stefan Ihle <stefanihle@gmx.net>
2023-06-20 14:05:27 +0200

    improve animation for icons

--- src/app/(site)/components/Header.tsx
+++ src/app/(site)/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/navigation';
 
 const Header = () => {
@@ -10,8 +11,26 @@ const Header = () => {
 
 	const handleNavigation = (route: string) => {
 		router.push(route);
-		setIsOpen(preIsOpenState => !preIsOpenState);
+		setIsOpen(prevIsOpenState => !prevIsOpenState);
 	};
+
+	const pathVariants = {
+		open: {
+			d: 'M6 18L18 6M6 6l12 12',
+			transition: { d: { duration: 0.3, ease: 'easeInOut' } },
+		},
+		closed: {
+			d: 'M4 6h16M4 12h16M4 18h16',
+			transition: { d: { duration: 0.3, ease: 'easeInOut' } },
+		},
+	};
+
+	const menuVariants = {
+		open: { opacity: 1, height: 'auto' },
+		closed: { opacity: 0, height: 0 },
+	};
+
+	const transition = { duration: 0.3, ease: 'easeInOut' };
 
 	return (
 		<header className='relative bg-slate-950 text-white'>
@@ -49,63 +68,54 @@ const Header = () => {
 							aria-expanded='false'
 						>
 							<span className='sr-only'>Open main menu</span>
-							{!isOpen ? (
-								<svg
-									className='block h-6 w-6'
-									xmlns='http://www.w3.org/2000/svg'
-									fill='none'
-									viewBox='0 0 24 24'
-									stroke='currentColor'
-									aria-hidden='true'
-								>
-									<path
-										strokeLinecap='round'
-										strokeLinejoin='round'
-										strokeWidth={2}
-										d='M4 6h16M4 12h16M4 18h16'
-									/>
-								</svg>
-							) : (
-								<svg
-									className='block h-6 w-6'
-									xmlns='http://www.w3.org/2000/svg'
-									fill='none'
-									viewBox='0 0 24 24'
-									stroke='currentColor'
-									aria-hidden='true'
-								>
-									<path
-										strokeLinecap='round'
-										strokeLinejoin='round'
-										strokeWidth={2}
-										d='M6 18L18 6M6 6l12 12'
-									/>
-								</svg>
-							)}
+							<motion.svg
+								className='block h-6 w-6'
+								xmlns='http://www.w3.org/2000/svg'
+								viewBox='0 0 24 24'
+								fill='none'
+								stroke='currentColor'
+							>
+								<motion.path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									strokeWidth={2}
+									variants={pathVariants}
+									initial='closed'
+									animate={isOpen ? 'open' : 'closed'}
+								/>
+							</motion.svg>
 						</button>
 					</div>
 				</div>
 			</div>
-
-			<div
-				className={`${isOpen ? 'block' : 'hidden'} md:hidden`}
-				id='mobile-menu'
-			>
-				<div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-					<a
-						onClick={() => handleNavigation('/projects')}
-						className='uppercase text-semibold hover:text-rose-500 block px-3 py-2 rounded-md text-base font-medium'
+			<AnimatePresence>
+				{isOpen && (
+					<motion.div
+						className='md:hidden'
+						id='mobile-menu'
+						initial='closed'
+						animate='open'
+						exit='closed'
+						variants={menuVariants}
+						transition={transition}
 					>
-						Projects
-					</a>
-					<a
-						onClick={() => handleNavigation('/posts')}
-						className='uppercase text-semibold hover:text-rose-500 block px-3 py-2 rounded-md text-base font-medium'
-					>
-						Blog
-					</a>
-				</div>
-			</div>
+						<div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
+							<a
+								onClick={() => handleNavigation('/projects')}
+								className='uppercase text-semibold hover:text-rose-500 block px-3 py-2 rounded-md text-base font-medium'
+							>
+								Projects
+							</a>
+							<a
+								onClick={() => handleNavigation('/posts')}
+								className='uppercase text-semibold hover:text-rose-500 block px-3 py-2 rounded-md text-base font-medium'
+							>
+								Blog
+							</a>
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</header>
 	);
 };
